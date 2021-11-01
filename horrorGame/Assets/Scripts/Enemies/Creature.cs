a97d64b680871193d11695685b240c3985d57146
nicolas <nicolas.1701625295@ucaldas.edu.co>
2021-11-01 16:14:56 -0500

    Add attacking, tracking creature

--- horrorGame/Assets/Scripts/Enemies/Creature.cs
+++ horrorGame/Assets/Scripts/Enemies/Creature.cs
@@ -6,27 +6,21 @@ public class Creature : MonoBehaviour
 {
 
     private Animator anim;
-	//private CharacterController controller;
-	private int battle_state = 0;
-	public float speed = 6.0f;
-	public float runSpeed = 3.0f;
-	public float turnSpeed = 60.0f;
-	public float gravity = 20.0f;
-	private Vector3 moveDirection = Vector3.zero;
-	private float w_sp = 0.0f;
-	private float r_sp = 0.0f;
 
     private Vector3 endPoint;
     private Vector3 startPoint;
     private Vector3 nextPoint;
     private bool flagDirection = true;
+    private bool patrolling = true;
 
     // Start is called before the first frame update
     void Start()
     {
+
+       
         anim = GetComponent<Animator>();
         //w_sp = speed; //read walk speed
-		r_sp = runSpeed; //read run speed
+		//r_sp = runSpeed; //read run speed
 		//controller = GetComponent<CharacterController> ();
 
         endPoint = GameObject.Find("point1").gameObject.transform.position;
@@ -37,23 +31,34 @@ public class Creature : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //anim.SetInteger ("moving", 0); // idle
-        anim.SetInteger ("moving", 1);//walk
 
-		//runSpeed = 1;
-        float distance = Vector3.Distance(this.transform.position,nextPoint);
+        if(patrolling){
 
-        if(distance < 1.0){
-            if (flagDirection)
-            {
-                nextPoint = startPoint;
-                flagDirection = false; 
-            }
-            else{
-                nextPoint = endPoint;
-                flagDirection = true;
+            anim.SetInteger("moving",1); /// walk
+            float distance = Vector3.Distance(this.transform.position,nextPoint);
+
+            if(distance < 1.0){
+               if (flagDirection)
+               {
+                   nextPoint = startPoint;
+                   flagDirection = false; 
+                }
+                else{
+                    nextPoint = endPoint;
+                    flagDirection = true;
+                }
+                
             }
             transform.LookAt(nextPoint); 
-        }
+            
+        }        
+    }
+
+    public bool GetPatrolling(){
+        return this.patrolling;
+    }
+
+    public void SetPatrolling(bool newPatrolling){
+        this.patrolling = newPatrolling;
     }
 }
---
+++ horrorGame/Assets/Scripts/Enemies/Tracking.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//script de seguimiento del enemigo al jugador
+public class Tracking : MonoBehaviour
+{
+
+    private Animator anim;
+
+    public GameObject Enemy;
+    private bool vision;
+    private UnityEngine.AI.NavMeshHit hit;
+    private bool stop = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        anim.SetInteger("attack",0); //no attack;
+
+        vision = UnityEngine.AI.NavMesh.Raycast(transform.position, Enemy.transform.position, out hit, UnityEngine.AI.NavMesh.AllAreas);
+
+
+
+        // atacar desde cierta distancia
+        if(hit.distance <= 4f && !vision){
+            Hit();
+        }
+        else{
+            stop = false;
+        }
+
+
+        // perseguirlo si ha sido visto
+        if(hit.distance < 20f && !vision && !stop){
+            this.gameObject.GetComponent<Creature>().SetPatrolling(false);
+            Follow();
+        }
+        // si ya está muy lejos el jugador y no puede verlo, seguir patrullando
+        if(hit.distance >= 20f){
+            this.gameObject.GetComponent<Creature>().SetPatrolling(true);
+        }
+
+        
+    }
+
+    void Follow(){
+
+        anim.SetInteger("moving",2); //run
+        this.GetComponent<AudioSource>().Play();
+        transform.LookAt(Enemy.transform.position); 
+    }
+
+    void Hit(){
+        anim.SetInteger("attack",1);
+    }
+
+    void Stop(){
+        anim.SetInteger("moving",0);//idle
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.name == "FPSController")
+        {
+            Stop();
+            stop = true;
+            print("quitar vida");
+            //this.GetComponent<AudioSource>().Play();
+        }
+    }
+}
